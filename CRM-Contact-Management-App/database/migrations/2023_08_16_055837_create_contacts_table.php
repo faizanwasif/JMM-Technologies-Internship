b0a2dc09285447b3e73210394c5376d7e0e86180
faizanwasif <faizanwasif102@gmail.com>
2023-08-16 11:37:15 +0500

    migrations, models, controllers and seeders files made

---
+++ CRM-Contact-Management-App/database/migrations/2023_08_16_055837_create_contacts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100)->nullable(false);
+            $table->string('email', 100)->nullable(false);
+            $table->string('phone', 20)->nullable(false);
+            $table->string('company', 100)->nullable(false);
+            $table->unsignedBigInteger('user_id')->nullable(false);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('tag_id')->nullable(true)->default(null);
+            $table->foreign('tag_id')->references('id')->on('tags');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
---
+++ CRM-Contact-Management-App/database/seeders/ContactSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+use App\Models\Contact;
+
+class ContactSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create();
+        
+        for($i=0; $i<50; $i++){
+            DB::table('contacts')->insert([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'phone' => $faker->phoneNumber,
+                'company' => $faker->company,
+                'user_id' => 1,
+            ]);
+
+        }   
+    }
+}
